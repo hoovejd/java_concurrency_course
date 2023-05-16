777222a752e0e7cffc7c35d03a4d3858b4e9e767
Jarrod Hoover <hoov85@gmail.com>
2023-05-16 14:07:10 -0400

    added interrupter

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # java_concurrency_course
 
-This repo contains the code from the Udemy course (Java Multithreading, Concurrency &amp; Performance Optimization)[https://www.udemy.com/course/java-multithreading-concurrency-performance-optimization]
+This repo contains the code from the Udemy course [Java Multithreading, Concurrency &amp; Performance Optimization](https://www.udemy.com/course/java-multithreading-concurrency-performance-optimization)
---
+++ multithreading_3/src/App1.java
@@ -0,0 +1,22 @@
+public class App1 {
+    public static void main(String[] args) throws Exception {
+        Thread thread = new Thread(new BlockingTask());
+
+        thread.start();
+
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(500000);
+            } catch (InterruptedException e) {
+                System.out.println(("Exiting blocking thread"));
+            }
+
+        }
+    }
+}
