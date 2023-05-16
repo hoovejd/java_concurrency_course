2a72f4b97a78f802d7a374dd8a9e37f4c99905b6
Jarrod Hoover <hoov85@gmail.com>
2023-05-16 12:34:03 -0400

    Added App2

--- multithreading_1/multithreading/src/App1.java
+++ multithreading_1/multithreading/src/App1.java
@@ -1,4 +1,4 @@
-public class App {
+public class App1 {
     public static void main(String[] args) throws Exception {
 
         Thread thread = new Thread(new Runnable() {
---
+++ multithreading_1/multithreading/src/App2.java
@@ -0,0 +1,16 @@
+public class App2 {
+
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+        thread.start();
+    }
+
+    private static class NewThread extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("Hello from " + Thread.currentThread().getName());
+        }
+    }
+
+}
